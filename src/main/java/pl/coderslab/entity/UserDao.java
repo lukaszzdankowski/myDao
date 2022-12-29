868db7aa86999c6e5074e357f1d984af76a96c7c
Łukasz Zdankowski <lukasz.zdankowski@gmail.com>
2022-12-29 19:54:22 +0100

    User class and Queries

--- src/main/java/pl/coderslab/entity/UserDao.java
+++ src/main/java/pl/coderslab/entity/UserDao.java
@@ -1,2 +1,16 @@
-package pl.coderslab.entity;public class UserDao {
+package pl.coderslab.entity;
+
+public class UserDao {
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+    private static final String READ_USER_QUERY =
+            "SELECT * FROM users WHERE id = ?";
+    private static final String UPDATE_USER_QUERY =
+            "UPDATE users SET email = ?, username = ?, password = ? WHERE id = ?";
+
+    private static final String DELETE_USER_QUERY =
+            "DELETE FROM users WHERE id = ?";
+    private static final String FIND_ALL_USER_QUERY =
+            "SELECT * FROM users";
+
 }
